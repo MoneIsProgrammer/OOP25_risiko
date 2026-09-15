9cdd19187aab73de056607f43932249612219ff3
Marco Zangheri <marcozangheri44@gmail.com>
2026-09-15 15:58:58 +0200

    added the map to the game screen

--- src/main/java/it/unibo/risiko/controller/GameController.java
+++ src/main/java/it/unibo/risiko/controller/GameController.java
@@ -9,7 +9,12 @@ import it.unibo.risiko.model.player.Player;
 import it.unibo.risiko.model.player.PlayerRequest;
 import it.unibo.risiko.model.player.Roster;
 import it.unibo.risiko.model.player.RosterImpl;
+import it.unibo.risiko.view.map.MapCanvas;
+import it.unibo.risiko.view.map.MapLayout;
+import it.unibo.risiko.view.map.MapView;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -18,6 +23,9 @@ import javafx.stage.Stage;
  * GameController
  */
 public class GameController {
+
+    private static final double WINDOW_WIDTH = 1100;
+    private static final double WINDOW_HEIGHT = 700;
 
     Roster roster;
     GameMap map;
@@ -32,12 +40,33 @@ public class GameController {
     }
 
     public void start(Stage stage) {
-        // TODO Auto-generated method stub, start the view here by putting the scene in mainstage
+        final MapCanvas canvas;
+        try {
+            canvas = new MapCanvas(map, MapLayout.loadDefault());
+        } catch (final IOException e) {
+            throw new IllegalStateException("Could not load the map layout", e);
+        }
+
+        // from here we use it only as a MapView
+        final MapView mapView = canvas;
+        for (final Player player : roster.getAllPlayers()) {
+            mapView.setPlayerColor(player.getId(), player.getColor());
+        }
+
         var box = new VBox();
         for (Player player : roster.getAllPlayers()) {
             box.getChildren().add(new Text(player.getName() + player.getId() + player.getColor().name()));
         }
-        stage.setScene(new Scene(box));
+
+        // the map follows the size of the window
+        final var container = new Pane(canvas);
+        canvas.widthProperty().bind(container.widthProperty());
+        canvas.heightProperty().bind(container.heightProperty());
+
+        final var root = new BorderPane();
+        root.setCenter(container);
+        root.setRight(box);
+        stage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
     }
 
 }
