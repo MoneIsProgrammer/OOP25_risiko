701557daad84676eb385d5ac5e615078fa3f764b
Marco Zangheri <marcozangheri44@gmail.com>
2026-09-15 15:03:01 +0200

    added Phase enum with the phases of the turn

---
+++ src/main/java/it/unibo/risiko/model/turn/Phase.java
@@ -0,0 +1,20 @@
+package it.unibo.risiko.model.turn;
+
+/**
+ * The phase the turn is in. The turn manager changes it, the map needs it to know what
+ * a click is for.
+ */
+public enum Phase {
+
+    /** Start of the game, everyone places their first armies. */
+    SETUP,
+
+    /** Start of the turn, you place your new armies. */
+    REINFORCE,
+
+    /** You attack the territories next to yours. */
+    ATTACK,
+
+    /** End of the turn, you can move armies between your territories. */
+    MOVE
+}
