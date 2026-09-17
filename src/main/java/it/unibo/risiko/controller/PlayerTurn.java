3b71a3e7123307daea2cb8b04a80db649788ab82
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-17 09:54:30 +0200

    Fixed Checkstyle errors

--- src/main/java/it/unibo/risiko/controller/PlayerTurn.java
+++ src/main/java/it/unibo/risiko/controller/PlayerTurn.java
@@ -7,21 +7,38 @@ import java.util.List;
 import it.unibo.risiko.model.player.Player;
 import it.unibo.risiko.model.player.Roster;
 
-public class PlayerTurn {
+/**
+ * Handles the player turn order and their removal from the game.
+ */
+public final class PlayerTurn {
 
-    List<Player> playerOrder = new ArrayList<>();
-    int counter = -1;
-    
+    private List<Player> playerOrder = new ArrayList<>();
+    private int counter = -1;
+    /**
+     * Default constructor, already shuffles the players.
+     * 
+     * @param roster The players to shuffle
+     */
     public PlayerTurn(final Roster roster) {
         playerOrder.addAll(roster.getAllPlayers());
         Collections.shuffle(playerOrder);
     }
 
+    /**
+     * Gets the next player in turn order.
+     * 
+     * @return the next player
+     */
     public Player next() {
         counter++;
         return playerOrder.get(counter % playerOrder.size());
     }
 
+    /**
+     * Removes a player for from the turn order.
+     * 
+     * @param player to remove
+     */
     public void remove(final Player player) {
         final var current = playerOrder.get(counter % playerOrder.size());
         if (current.equals(player)) {
@@ -32,5 +49,4 @@ public class PlayerTurn {
         }
         counter = playerOrder.indexOf(current);
     }
-
 }
