33750c2c66fbcd8c17cd38baf13584d7145f42a7
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-18 15:50:23 +0200

    Phase now start on setup

--- src/main/java/it/unibo/risiko/controller/PlayerTurn.java
+++ src/main/java/it/unibo/risiko/controller/PlayerTurn.java
@@ -17,7 +17,7 @@ public final class PlayerTurn {
     private boolean setupDone = false;
     private List<Player> playerOrder = new ArrayList<>();
     private int counter = -1;
-    private Phase currentPhase;
+    private Phase currentPhase = Phase.SETUP;
     private final PropertyChangeSupport phaseChange = new PropertyChangeSupport(this);
 
 
