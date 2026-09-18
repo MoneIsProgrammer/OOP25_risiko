3326e4154c5dbde1d0b1e6683062b450d00f550d
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-18 14:18:41 +0200

    player turn now manages the phase

--- src/main/java/it/unibo/risiko/controller/PlayerTurn.java
+++ src/main/java/it/unibo/risiko/controller/PlayerTurn.java
@@ -6,14 +6,16 @@ import java.util.List;
 
 import it.unibo.risiko.model.player.Player;
 import it.unibo.risiko.model.player.Roster;
+import it.unibo.risiko.model.turn.Phase;
 
 /**
  * Handles the player turn order and their removal from the game.
  */
 public final class PlayerTurn {
-
+    private boolean setupDone = false;
     private List<Player> playerOrder = new ArrayList<>();
     private int counter = -1;
+    private Phase currentPhase;
 
     /**
      * Default constructor, already shuffles the players.
@@ -32,6 +34,9 @@ public final class PlayerTurn {
      */
     public Player next() {
         counter++;
+        if (setupDone) {
+            this.currentPhase = Phase.PLAYCARDS;
+        }
         return playerOrder.get(counter % playerOrder.size());
     }
 
@@ -49,5 +54,48 @@ public final class PlayerTurn {
             throw new IllegalArgumentException("player does not exist");
         }
         counter = playerOrder.indexOf(current);
+    }
+
+    /**
+     * Advaces to the next phase.
+     */
+    public void advancePhase() {
+        switch (currentPhase) {
+            case PLAYCARDS:
+                setPhase(Phase.REINFORCE);
+                break;
+            case REINFORCE:
+                setPhase(Phase.ATTACK);
+                break;
+            case ATTACK:
+                setPhase(Phase.MOVE);
+                break;
+            default:
+                //do nothing
+                break;
+        }
+    }
+
+    /**
+     * Sets the current game phase.
+     * 
+     * @param phase the new phase
+     */
+    public void setPhase(Phase phase) {
+        this.currentPhase = phase;
+    }
+
+    /**
+     * return the current game phase.
+     * 
+     * @return the current game phase
+     */
+    public Phase getCurrentPhase() {
+        return this.currentPhase;
+    }
+
+    public void setupFinished() {
+        setPhase(Phase.PLAYCARDS);
+        setupDone = true;
     }
 }
