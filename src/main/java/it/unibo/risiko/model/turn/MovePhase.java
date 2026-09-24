9d17ad32d7dddc8f8200531762bcd287d348f587
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-24 11:19:57 +0200

    fixed checkstyle and Pmd errors

--- src/main/java/it/unibo/risiko/model/turn/MovePhase.java
+++ src/main/java/it/unibo/risiko/model/turn/MovePhase.java
@@ -10,7 +10,10 @@ import it.unibo.risiko.model.player.Roster;
 import it.unibo.risiko.model.player.strategy.HumanStrategy;
 import it.unibo.risiko.model.player.strategy.PlayerStrategy;
 
-public class MovePhase{
+/**
+ * Models the move phase of the game.
+ */
+public class MovePhase {
     private final GameMap map;
     private final Roster players;
     private final VictoryCheck check;
@@ -18,9 +21,27 @@ public class MovePhase{
     private Player currentPlayer;
     private PlayerStrategy strategy;
     private HumanStrategy humanStrategy;
-    private boolean isCompleted = false;
+    private boolean isCompleted;
     private Player winner;
 
+    /**
+     * Default constructor.
+     * 
+     * @param map the game map
+     * @param players the roster of players
+     * @param check the checker for victory
+     * @param turn the current turn
+     */
+    public MovePhase(final GameMap map, final Roster players, final VictoryCheck check, final PlayerTurn turn) {
+        this.map = map;
+        this.players = players;
+        this.check = check;
+        this.turn = turn;
+    }
+
+    /**
+     * Checks if the phase is done.
+     */
     public void phaseStart() {
         moveArmies();
         if (checkElimination()) {
@@ -31,17 +52,18 @@ public class MovePhase{
         isCompleted = true;
     }
 
-    public boolean isCompleted () {
+    /** 
+     * To check if current phase is completed.
+     * 
+     * @return true if the phase is completed
+     */
+    public boolean isCompleted() {
         return isCompleted;
     }
 
-    public MovePhase (final GameMap map, final Roster players, final VictoryCheck check, final PlayerTurn turn) {
-        this.map = map;
-        this.players = players;
-        this.check = check;
-        this.turn = turn;
-    }
-
+    /**
+     * Moves armies.
+     */
     void moveArmies() {
         currentPlayer = turn.getCurrentPlayer();
         if (!(currentPlayer.isHuman())) {
@@ -55,56 +77,59 @@ public class MovePhase{
         }
     }
 
-
-    /** After the player has moved armies between territories, 
+    /** 
+     * After the player has moved armies between territories, 
      * check whether they've achieved their objective or has a 
-     * player been eliminated
+     * player been eliminated.
+     * 
+     * @return true if a player has been eliminated
      */
-
     public boolean checkElimination() {
-        for (Player player: players.getAllPlayers()) {
+        for (final Player player: players.getAllPlayers()) {
             if (map.getTerritoriesOf(player.getId()).size() <= 0) {
-                /** Check whether a player has been eliminated and if 
+                /* Check whether a player has been eliminated and if 
                  * the current player's objective was to eliminate this 
                  * player, then it returns true, which means the current 
                  * player has completed their objective and won the game.
                  * 
                  */
-                return (managePlayerElimination(player, currentPlayer));
+                return managePlayerElimination(player, currentPlayer);
             }
         }
-        /** If none of the players got eliminated or if a player got eliminated 
+        /* If none of the players got eliminated or if a player got eliminated 
          * and this was not the objective of the current player, check if the 
          * player has achieved their objective
         */
-        return (check.victoryCheck(currentPlayer));
+        return check.victoryCheck(currentPlayer);
     }
-
 
     /**
      * Manages the elimination of a player, by checking whether their 
-     * elimination was someone's objective
-     * If it was the objective of the current player, returns true
+     * elimination was someone's objective.
      * If it was the objective of another player, changes the objective 
      * of said player
      * In all the other cases, returns false
+     * 
+     * @param playerEliminated the eliminated player
+     * @param actualPlayer the acting player
+     * @return If it was the objective of the current player, returns true
      */
-    boolean managePlayerElimination(Player playerEliminated, Player currentPlayer){
-        RisikoColors eliminatedColor = playerEliminated.getColor();
+    boolean managePlayerElimination(final Player playerEliminated, final Player actualPlayer) {
+        final RisikoColors eliminatedColor = playerEliminated.getColor();
 
-        /** objectiveEliminateColor() checks whether a player's objective was to eliminate 
+        /* objectiveEliminateColor() checks whether a player's objective was to eliminate 
          * eliminated color's troop, if the current player's objective was to eliminate the 
          * eliminated player -> current player wins */
-        if (objectiveEliminateColor(currentPlayer, eliminatedColor)) {
+        if (objectiveEliminateColor(actualPlayer, eliminatedColor)) {
             return true;
         }
 
-        /** Loop through all the players and check whether a player's (a player other tha current player) 
+        /* Loop through all the players and check whether a player's (a player other tha current player) 
          * objective was to eliminate eliminated colour, if it was set a new objective (conquer 24 
          * territories) for that player
          */
-        for (Player player: players.getAllPlayers()) {
-            if ( !((player).equals(currentPlayer)) && objectiveEliminateColor(player, eliminatedColor) ) {
+        for (final Player player: players.getAllPlayers()) {
+            if (!(player.equals(actualPlayer)) && objectiveEliminateColor(player, eliminatedColor)) {
                 player.setNewObjective();
             }
         }
@@ -115,29 +140,46 @@ public class MovePhase{
     }
 
     /**
-     * check whether the player's objective was eliminating eliminated colour
+     * check whether the player's objective was eliminating eliminated colour.
+     * 
      * @param player the player for whose objective is being checked
      * @param eliminatedColor the color of the eliminated player
-     * @return returns false if eliminating the colour was not the player's 
-     * objective, returns true if it was the player's objective
+     * 
+     * @return returns false if eliminating the colour was not the player's objective,
+     *      returns true if it was the player's objective
      */
-    boolean objectiveEliminateColor(Player player, RisikoColors eliminatedColor) {
-        Card playerObjective = player.getObjective();
+    boolean objectiveEliminateColor(final Player player, final RisikoColors eliminatedColor) {
+        final Card playerObjective = player.getObjective();
         /* controllo se l'obiettivo del player era eliminare quel colore */
-        return (eliminateColorObjective(playerObjective.getObjective())) && (playerTargetColour(playerObjective.getObjective()) == eliminatedColor);
+        return
+            eliminateColorObjective(playerObjective.getObjective())
+            && playerTargetColour(playerObjective.getObjective()) == eliminatedColor;
     }
 
-    /** Check whether a player's objective was between objectives 9 and 14, 
+    /** 
+     * Check whether a player's objective was between objectives 9 and 14, 
      * i.e. to eliminate a certain colour troop
-     * @return returns true if it was any of the colour objectives
-     * returns false otherwise
+     * 
+     * @param objective the objective to check
+     * @return true if it was any of the colour objectives false otherwise
      */
-    boolean eliminateColorObjective(Objective objective) {
-        return ( (objective.equals(Objective.OBJECTIVE9)) || (objective.equals(Objective.OBJECTIVE10)) || (objective.equals(Objective.OBJECTIVE11)) || (objective.equals(Objective.OBJECTIVE12)) || (objective.equals(Objective.OBJECTIVE13)) || (objective.equals(Objective.OBJECTIVE14)) );
+    boolean eliminateColorObjective(final Objective objective) {
+        return objective.equals(Objective.OBJECTIVE9) 
+        || objective.equals(Objective.OBJECTIVE10) 
+        || objective.equals(Objective.OBJECTIVE11) 
+        || objective.equals(Objective.OBJECTIVE12) 
+        || objective.equals(Objective.OBJECTIVE13) 
+        || objective.equals(Objective.OBJECTIVE14);
     }
 
-    /* Returns the colour of the troop who the player has to eliminate */
-    RisikoColors playerTargetColour(Objective objective) {
+    /**
+     *  Returns the colour of the troop who the player has to eliminate.
+     * 
+     * @param objective the objective of the player
+     * 
+     * @return the color of the target
+     */
+    RisikoColors playerTargetColour(final Objective objective) {
         switch (objective) {
             case OBJECTIVE9:
                 return RisikoColors.YELLOW;
