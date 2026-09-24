8779e6a21a35ceb43de7e1f7e7b03729f54e21d4
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-24 10:35:10 +0200

    fixed checkstyle and Pmd errors

--- src/main/java/it/unibo/risiko/model/deck/TerritoriesDeck.java
+++ src/main/java/it/unibo/risiko/model/deck/TerritoriesDeck.java
@@ -1,26 +1,27 @@
 package it.unibo.risiko.model.deck;
 
 /**
- * This is a sub-class, it is a specialized type of the Deck Class
+ * This is a sub-class, it is a specialized type of the Deck Class.
  * Attributes: cards (already inherited from Deck)
  * Methods: createDeck(), addJollyDeck()
- * 
  * The purpose of this class is to create a Deck with 
  * all the territory cards
  **/
 public class TerritoriesDeck extends Deck {
 
-    /* Create the deck with all the territory cards */
-    public void createTerritoryDeck () {
+    /**
+     * Create the deck with all the territory cards.
+     */
+    public void createTerritoryDeck() {
         this.populateTerritoryDeck();
         this.shuffle();
     }
 
-    /* Add the Jolly cards to the deck that already contains all
-    the territory cards */
-    public void addJollyTerritoryDeck () {
+    /**
+     * Add the Jolly cards to the deck that already contains all the territory cards.
+     */
+    public void addJollyTerritoryDeck() {
         this.addJolly();
         this.shuffle();
     }
-    
 }
