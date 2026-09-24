1f67e587ef03a3d0c0d7a2132beee877ddf429c5
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-24 11:41:16 +0200

    fixed checkstyle and Pmd errors

--- src/main/java/it/unibo/risiko/model/turn/ReinforcePhase.java
+++ src/main/java/it/unibo/risiko/model/turn/ReinforcePhase.java
@@ -1,46 +1,54 @@
 package it.unibo.risiko.model.turn;
 
-import it.unibo.risiko.controller.PlayerTurn;
 import it.unibo.risiko.model.player.Player;
 import it.unibo.risiko.model.player.strategy.HumanStrategy;
-import it.unibo.risiko.model.player.strategy.PlayerStrategy;
 
 /**
  * This phase allows the current player to place their 
- * armies
- * ReinforcePhase
+ * armies.
  */
-public class ReinforcePhase{
+public class ReinforcePhase {
 
-    private PlayerTurn turn;
+    //private PlayerTurn turn;
     /* Receive current player from PlayerTurn */
-    private Player player = turn.getCurrentPlayer();
-    private PlayerStrategy strategy;
-    private HumanStrategy humanStrategy;
-    private boolean completed = false;
-    
+    private final Player player;
+    //private PlayerStrategy strategy;
+    //private HumanStrategy humanStrategy;
+    private boolean completed;
+
+    /**
+     * Default constructor.
+     * 
+     * @param player the current player
+     */
+    ReinforcePhase(final Player player) {
+        this.player = player;
+    }
+
+    /**
+     * Starts the phase.
+     */
     public void phaseStart() {
         placeArmies();
 
         completed = true;
     }
 
+    /**
+     * @return true if phase is finished
+     */
     public boolean isCompleted() {
         return completed;
     }
 
-    ReinforcePhase(final Player player) {
-        this.player = player;
-    }
-
     /**
      * Based on whether a player is ai or human, calls methods 
-     * to place armies as reinforcements
+     * to place armies as reinforcements.
      */
     private void placeArmies() {
         if (player.isHuman()) {
-            strategy = player.getStrategy();
-            humanStrategy = (HumanStrategy) strategy;
+            final var strategy = player.getStrategy();
+            final var humanStrategy = (HumanStrategy) strategy;
             if (humanStrategy.canCreateReinforce()) {
                 player.reinforce(player.getArmies());
             }
