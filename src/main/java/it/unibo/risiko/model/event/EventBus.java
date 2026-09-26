df4576e7dedce1d8a97386131a7448d7020727bc
Morri Simone <simone.morri2@studio.unibo.it>
2026-09-26 17:29:26 +0200

    fixed checkstyle and Pmd errors

--- src/main/java/it/unibo/risiko/model/event/EventBus.java
+++ src/main/java/it/unibo/risiko/model/event/EventBus.java
@@ -6,38 +6,44 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-/** Allows the controller to send an alert to the view.
+/** 
+ * Allows the controller to send an alert to the view.
  * In particular, the setWinner method in PlayerTurn 
  * publishes an event and the GameScene subscribes to it 
- * and calls the showGameOver method
- * EventBus
+ * and calls the showGameOver method.
  * 
-*/
+ * 
+ */
 public class EventBus {
     private final Map<Class<?>, List<Consumer<?>>> subscribers = new HashMap<>();
-    
+
     /**
-     * the GameOverEvent class is passed as the class to subscribe to
-     * @param <T>
+     * The GameOverEvent class is passed as the class to subscribe to.
+     * 
+     * @param <T> the type of event to be subscribed to
      * @param eventType the class to subscribe to
      * @param eventHandler when GameOverEvent is published, this function is called
      */
-    public <T> void subscribe(Class<T> eventType, Consumer<T> eventHandler) {
+    public <T> void subscribe(final Class<T> eventType, final Consumer<T> eventHandler) {
         subscribers.computeIfAbsent(eventType, k -> new ArrayList<>()).add(eventHandler);
     }
 
+    /** 
+     * Get all the handlers and if the handlers are null, return nothing. 
+     * Otherwise, for each handler call them for that event.
+     * 
+     * @param <T> type of event to be subscribed to
+     * @param event the event to be notified of.
+     */
     @SuppressWarnings("unchecked")
-    public <T> void publish(T event) {
-        /** get all the handlers and if the handlers are null, return nothing. 
-         * Otherwise, for each handler call them for that event
-        */
-        List<Consumer<?>> handlers = subscribers.get(event.getClass());
+    public <T> void publish(final T event) {
+        final List<Consumer<?>> handlers = subscribers.get(event.getClass());
 
         if (handlers == null) {
             return;
         }
 
-        for (Consumer<?> handler : handlers) {
+        for (final Consumer<?> handler : handlers) {
             ((Consumer<T>) handler).accept(event);
         }
     }
